fdfd4be03e7fc8b3bd0ef443589231d5003b551a
Vicpip <caballeropv9@gmail.com>
2024-11-29 01:06:51 -0600

    btns

--- app/src/main/java/com/example/tita/vinculo.kt
+++ app/src/main/java/com/example/tita/vinculo.kt
@@ -1,12 +1,15 @@
 package com.example.tita
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class vinculo : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,12 @@ class vinculo : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnpagar: Button = findViewById(R.id.btnPagar)
+        btnpagar.setOnClickListener{
+            val intent = Intent(this, pagar::class.java)
+            startActivity(intent)
         }
     }
 }
