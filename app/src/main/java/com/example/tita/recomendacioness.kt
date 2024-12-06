9f953774309ad9a61098b6074685932b3fb6d6f4
AuroraCarbajal03 <icarbajalm2000@alumno.ipn.mx>
2024-12-05 21:22:41 -0600

    recomendaciones_aurora2_videos1

--- app/src/main/java/com/example/tita/recomendacioness.kt
+++ app/src/main/java/com/example/tita/recomendacioness.kt
@@ -1,8 +1,10 @@
 package com.example.tita
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +15,29 @@ class recomendacioness : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_recomendacioness)
+
+        val video = findViewById<VideoView>(R.id.video1)
+        val uri: Uri = Uri.parse(
+            "android.resource://" + packageName + "/raw/video1"
+        )
+        video.setVideoURI(uri)
+        video.requestFocus()
+        video.resume()
+        video.start()
+
+        val video2 = findViewById<VideoView>(R.id.video1)
+        val uri2: Uri = Uri.parse(
+            "android.resource://" + packageName + "/raw/video2"
+        )
+        video2.setVideoURI(uri2)
+        video2.requestFocus()
+        video2.resume()
+        video2.start()
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
